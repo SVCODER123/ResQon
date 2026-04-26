0c788b771c2c8657585affcadae6fd6ec0efb3b2
SVCODER123 <Add Email>
2026-04-26 22:32:24 +0530

    changed the serverr.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const userRoutes      = require('./routes/users');
 
 const app  = express();
 const PORT = process.env.PORT || 5000;
-const path = require('path');
+
 // ── Middleware ────────────────────────────────────────────────────────────────
 app.use(cors());
 app.use(express.json());
